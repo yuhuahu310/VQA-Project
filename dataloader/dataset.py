71c41ca387db510410c0af0d6e46b666eb779c83
Tianyu Zhang <tianyuz2@andrew.cmu.edu>
2023-03-25 18:28:07 -0400

    Improved VQADataset QA loading (still need to change). LSTM partially done.
    Co-authored-by: Yinjia Liu (Franklin) <franklinlyj24@users.noreply.github.com>
    Co-authored-by: Hefei Tu <hefeit@andrew.cmu.edu>

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -1,6 +1,4 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 import os
 import skimage.io as io
@@ -9,7 +7,7 @@ from nltk.tokenize import RegexpTokenizer
 import numpy as np
 from vqa import VQA
 
-
+# TODO: add padding and field to tackle sos/eos
 class VQADataset(Dataset):
     def __init__(self, ds_path, phase):
         '''
@@ -20,16 +18,37 @@ class VQADataset(Dataset):
         self.vqa = VQA(annotation_file=os.path.join(ds_path, 'Annotations', f'{phase}.json'))
         # Get vocabulary using training data
         if phase == 'train':
-            self.vocab = self.vqa.get_vocab()
+            self.vocab = ["<sos>", "<eos>"] + self.vqa.get_vocab()
         else:
-            self.vocab = VQA(annotation_file=os.path.join(ds_path, 'Annotations/train.json')).get_vocab()
+            self.vocab = ["<sos>", "<eos>"] + VQA(annotation_file=os.path.join(ds_path, 'Annotations/train.json')).get_vocab()
+        # import pdb;pdb.set_trace()
+        self.vocab = dict(zip(self.vocab, np.arange(len(self.vocab))))
         self.ds_path = ds_path
         self.phase = phase
 
     def __len__(self):
         return len(self.vqa.dataset)
+    
+    def _sent_2_idx_seq(self, sent):
+        tokenizer = RegexpTokenizer(r'\w+')
+        vec = ['<sos>'] + [self.vocab[word] for word in tokenizer.tokenize(sent)] + ['<eos>']
+        return vec
+
+    def getQA(self, idx):
+        '''Return only questions and all answers as idx seqs
+        '''
+        qa_pair = self.vqa.dataset[idx]
+        answers = qa_pair['answers']
+        question = qa_pair['question']
+        q_vec = self._sent_2_idx_seq(question)
+
+        flat_answers = [i['answer'] for i in answers]
+        a_vec = [self._sent_2_idx_seq(ans) for ans in flat_answers]
+        return q_vec, a_vec
 
     def __getitem__(self, idx):
+        '''Return images, questions, and answers
+        '''
         qa_pair = self.vqa.dataset[idx]
         image_id = qa_pair['image']
         answers = qa_pair['answers']
@@ -42,12 +61,17 @@ class VQADataset(Dataset):
         img_tensor = torch.tensor(img_arr)
 
         # TODO: Convert answers and questions to one-hot vectors
-        answer_vocab = self.vqa.get_answers_vocab()
-        answer_vec = torch.zeros(len(answer_vocab))
-        for ans in answers:
-            ans_idx = answer_vocab[ans['answer']]
-            answer_vec[ans_idx] = 1
+        # answer_vocab = self.vqa.get_answers_vocab()
+        # answer_vec = torch.zeros(len(answer_vocab))
+        # for ans in answers:
+        #     ans_idx = answer_vocab[ans['answer']]
+        #     answer_vec[ans_idx] = 1
+
 
 
         return img_tensor
 
+if __name__ == '__main__':
+    vqa_dataset = VQADataset("../data", "train")
+    print(f'len of dataset is {len(vqa_dataset)}')
+    print(vqa_dataset.getQA(0))
--- unimodal_baseline/language/LSTM.py
+++ unimodal_baseline/language/LSTM.py
@@ -1,138 +1,102 @@
-from ...vqaTools.vqa import VQA
+import sys
+sys.path.insert(0, '../../dataloader')
+from dataset import VQADataset
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
+import random
 
-
-# class LSTM(nn.Module):
-#     def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, n_layers, dropout):
-#         super().__init__()
-        
-#         self.embedding = nn.Embedding(vocab_size, embedding_dim)
-#         self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=n_layers, dropout=dropout, batch_first=True)
-#         self.fc = nn.Linear(hidden_dim, output_dim)
-#         self.dropout = nn.Dropout(dropout)
-        
-#     def forward(self, text):
-#         embedded = self.dropout(self.embedding(text))
-#         output, (hidden, cell) = self.lstm(embedded)
-#         hidden = self.dropout(hidden[-1,:,:])
-#         return self.fc(hidden)
 
 # Set device to GPU if available, otherwise use CPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Define the encoder class
+
 class EncoderLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, embedding_dim, hidden_size, vocab_size, num_layers, dropout):
         super(EncoderLSTM, self).__init__()
         self.hidden_size = hidden_size
-        self.lstm = nn.LSTM(input_size, hidden_size)
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_size, num_layers)
 
-    def forward(self, input):
-        _, (hidden, cell) = self.lstm(input)
+    def forward(self, sentence):
+        embs = self.word_embeddings(sentence)
+        _, (hidden, cell) = self.lstm(embs)
         return hidden, cell
 
-# Define the decoder class
+
 class DecoderLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, embedding_dim, hidden_size, vocab_size, num_layers, dropout):
         super(DecoderLSTM, self).__init__()
         self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.lstm = nn.LSTM(input_size, hidden_size)
-        self.linear = nn.Linear(hidden_size, output_size)
+        self.vocab_size = vocab_size
+        self.num_layers = num_layers
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_size, num_layers)
+        self.linear = nn.Linear(hidden_size, vocab_size)
 
     def forward(self, input, hidden, cell):
-        output, (hidden, cell) = self.lstm(input, (hidden, cell))
+        input = input.unsqueeze(0)
+        embs = self.embedding(input)
+        output, (hidden, cell) = self.lstm(embs, (hidden, cell))
         output = self.linear(output.squeeze(0))
         return output, hidden, cell
 
-# Define the Seq2Seq class that combines the encoder and decoder
+
 class Seq2Seq(nn.Module):
-    def __init__(self, encoder, decoder):
+    def __init__(self, encoder: EncoderLSTM, decoder: DecoderLSTM, device):
         super(Seq2Seq, self).__init__()
         self.encoder = encoder
         self.decoder = decoder
+        self.device = device
+        assert encoder.hidden_size == decoder.hidden_size
+        assert encoder.num_layers == decoder.num_layers
 
-    def forward(self, input, target):
+    def forward(self, input, target, teacher_forcing_ratio=0.5):
+        '''
+        :param target of size [target_len, batch_size]
+        '''
+        batch_size = target.shape[1]
+        target_len = target.shape[0]
+        outputs = torch.zeros(target_len, batch_size, self.decoder.vocab_size).to(self.device)
         encoder_hidden, encoder_cell = self.encoder(input)
 
-        decoder_input = torch.zeros((1, 1, self.decoder.input_size)).to(device)
+        decoder_input = target[0,:]
         decoder_hidden = encoder_hidden
         decoder_cell = encoder_cell
 
         loss = 0
-        for i in range(target.shape[0]):
+        for i in range(1, target_len):
             decoder_output, decoder_hidden, decoder_cell = self.decoder(decoder_input, decoder_hidden, decoder_cell)
             loss += nn.functional.cross_entropy(decoder_output, target[i])
-            decoder_input = target[i].unsqueeze(0).unsqueeze(0)
-
+            outputs[i] = decoder_output
+            teacher_forcing = random.random() < teacher_forcing_ratio
+            decoder_input = target[i] if teacher_forcing else decoder_output.argmax(dim=1)
+            
         return loss
 
+vqa_dataset = VQADataset("../../data", "train")
 
-# Hyperparameters
-BATCH_SIZE = 32
-EMBEDDING_DIM = 100
-HIDDEN_DIM = 256
-OUTPUT_DIM = 2 # binary classification
+VOCAB_SIZE = len(vqa_dataset.vocab)
+ENC_EMB_DIM = 256
+DEC_EMB_DIM = 256
+HID_DIM = 512
 N_LAYERS = 2
-DROPOUT = 0.5
-LEARNING_RATE = 0.001
-N_EPOCHS = 10
+ENC_DROPOUT = 0.5
+DEC_DROPOUT = 0.5
 
-# Load dataset
-vqa = VQA(annotation_file='v2_mscoco_train2014_annotations.json')
-anns = vqa.getAnns(ansTypes=['yes/no'])
-questions = [ann['question'] for ann in anns]
-answers = [ann['answers'][0]['answer'] for ann in anns] # TODO: deal with multiple answers
-data = list(zip(questions, answers))
-train_data, val_data = data[:int(0.8*len(data))], data[int(0.8*len(data)):]
+enc = EncoderLSTM(ENC_EMB_DIM, HID_DIM, VOCAB_SIZE, N_LAYERS, ENC_DROPOUT)
+dec = DecoderLSTM(DEC_EMB_DIM, HID_DIM, VOCAB_SIZE, N_LAYERS, DEC_DROPOUT)
 
-# Tokenize text
-from nltk.tokenize import word_tokenize
-from collections import Counter
+model = Seq2Seq(enc, dec, device).to(device)
 
-words = [word_tokenize(q.lower()) for q, a in train_data]
-word_freq = Counter([w for q in words for w in q])
-vocab = sorted(word_freq, key=word_freq.get, reverse=True)
-vocab_to_int = {word: i+1 for i, word in enumerate(vocab)}
-train_tokens = [[vocab_to_int[w] for w in q] for q in words]
-train_labels = [1 if a == 'yes' else 0 for q, a in train_data]
-
-# Create PyTorch DataLoader
-class VQADataset(Dataset):
-    def __init__(self, vqa):
-        self.vqa = vqa
+def init_weights(m):
+    for name, param in m.named_parameters():
+        nn.init.uniform_(param.data, -0.08, 0.08)
         
-    def __len__(self):
-        return len(self.vqa.dataset)
-    
-    def __getitem__(self, idx):
-        qa_pair = self.vqa.dataset[idx]
-        question = qa_pair['question']
-        image_id = qa_pair['image']
-        answers = qa_pair['answers']
-        
-        # TODO: preprocess question and answers
-        
-        # Convert question and image ID to tensors
-        question_tensor = torch.tensor(question_embedding) # replace with your own embedding function
-        image_tensor = torch.tensor(get_image_features(image_id)) # replace with your own image feature extraction function
-        
-        # Convert answers to one-hot vectors
-        answer_vocab = self.vqa.get_answers_vocab()
-        answer_vec = torch.zeros(len(answer_vocab))
-        for ans in answers:
-            ans_idx = answer_vocab[ans['answer']]
-            answer_vec[ans_idx] = 1
-        
-        return question_tensor, image_tensor, answer_vec
-
-# Instantiate VQA class and VQADataset
-vqa = VQA(annotation_file='/path/to/vqa/annotation/file.json')
-vqa_dataset = VQADataset(vqa)
-
-# Instantiate DataLoader
-batch_size = 32
-vqa_dataloader = DataLoader(vqa_dataset, batch_size=batch_size, shuffle=True)
+model.apply(init_weights)
+optimizer = optim.Adam(model.parameters())
+TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
+criterion = nn.CrossEntropyLoss(ignore_index = TRG_PAD_IDX)
