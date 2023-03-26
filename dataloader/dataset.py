fc157b6c08e9bfa25ca6a3870d9c4b8781009204
Tianyu Zhang <tianyuz2@andrew.cmu.edu>
2023-03-26 17:04:33 -0400

    Implemented and trained LSTM unimodal baseline
    Co-authored-by: Yinjia Liu (Franklin) <franklinlyj24@users.noreply.github.com>
    Co-authored-by: Hefei Tu <hefeit@andrew.cmu.edu>

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -7,6 +7,11 @@ from nltk.tokenize import RegexpTokenizer
 import numpy as np
 from vqa import VQA
 
+SOS_TOKEN = "<sos>"
+EOS_TOKEN = "<eos>"
+PAD_TOKEN = "<pad>"
+OOV_TOKEN = "<oov>"
+
 # TODO: add padding and field to tackle sos/eos
 class VQADataset(Dataset):
     def __init__(self, ds_path, phase):
@@ -18,10 +23,11 @@ class VQADataset(Dataset):
         self.vqa = VQA(annotation_file=os.path.join(ds_path, 'Annotations', f'{phase}.json'))
         # Get vocabulary using training data
         if phase == 'train':
-            self.vocab = ["<sos>", "<eos>"] + self.vqa.get_vocab()
+            self.vocab = np.concatenate(([SOS_TOKEN, EOS_TOKEN, PAD_TOKEN, OOV_TOKEN], self.vqa.get_vocab()))
         else:
-            self.vocab = ["<sos>", "<eos>"] + VQA(annotation_file=os.path.join(ds_path, 'Annotations/train.json')).get_vocab()
+            self.vocab = np.concatenate(([SOS_TOKEN, EOS_TOKEN, PAD_TOKEN, OOV_TOKEN], VQA(annotation_file=os.path.join(ds_path, 'Annotations/train.json')).get_vocab()))
         # import pdb;pdb.set_trace()
+        self.reverse_vocab = dict(list(enumerate(self.vocab)))
         self.vocab = dict(zip(self.vocab, np.arange(len(self.vocab))))
         self.ds_path = ds_path
         self.phase = phase
@@ -31,20 +37,14 @@ class VQADataset(Dataset):
     
     def _sent_2_idx_seq(self, sent):
         tokenizer = RegexpTokenizer(r'\w+')
-        vec = ['<sos>'] + [self.vocab[word] for word in tokenizer.tokenize(sent)] + ['<eos>']
+        vec = [self.vocab[SOS_TOKEN]] + [self.vocab[word] if word in self.vocab else self.vocab[OOV_TOKEN] for word in tokenizer.tokenize(sent)] + [self.vocab[EOS_TOKEN]]
         return vec
-
-    def getQA(self, idx):
-        '''Return only questions and all answers as idx seqs
-        '''
-        qa_pair = self.vqa.dataset[idx]
-        answers = qa_pair['answers']
-        question = qa_pair['question']
-        q_vec = self._sent_2_idx_seq(question)
-
-        flat_answers = [i['answer'] for i in answers]
-        a_vec = [self._sent_2_idx_seq(ans) for ans in flat_answers]
-        return q_vec, a_vec
+    
+    def idx_seq_2_sent(self, seq, to_str=True):
+        if to_str:
+            return ' '.join([self.reverse_vocab[idx] for idx in seq if self.reverse_vocab[idx] not in [SOS_TOKEN, EOS_TOKEN, PAD_TOKEN]])
+        else:
+            return [self.reverse_vocab[idx] for idx in seq]
 
     def __getitem__(self, idx):
         '''Return images, questions, and answers
@@ -55,7 +55,7 @@ class VQADataset(Dataset):
         question = qa_pair['question']
 
         # Get image and convert to tensor
-        img_fpath = os.path.join(self.ds_path, self.phase, image_id)
+        img_fpath = os.path.join(self.ds_path, "Images", self.phase, image_id)
         img_arr = io.imread(img_fpath)
         img_arr = resize(img_arr, (800, 600))
         img_tensor = torch.tensor(img_arr)
@@ -66,10 +66,31 @@ class VQADataset(Dataset):
         # for ans in answers:
         #     ans_idx = answer_vocab[ans['answer']]
         #     answer_vec[ans_idx] = 1
-
-
-
         return img_tensor
+    
+class QADataset(VQADataset):
+    def __init__(self, ds_path, phase):
+        super().__init__(ds_path, phase)
+
+    def __getitem__(self, idx):
+        '''Return only questions and all answers as idx seqs
+        '''
+        qa_pair = self.vqa.dataset[idx]
+        answers = qa_pair['answers']
+        question = qa_pair['question']
+        q_vec = self._sent_2_idx_seq(question)
+
+        flat_answers = [i['answer'] for i in answers]
+        # In case of a tie, pick the first. Might consider pick randomly
+        answer = max(flat_answers, key=flat_answers.count) 
+        a_vec = self._sent_2_idx_seq(answer)
+        return torch.tensor(q_vec, dtype=torch.int), torch.tensor(a_vec, dtype=torch.int)
+
+
+def collate_fn_pad(batch):
+    questions = torch.nn.utils.rnn.pad_sequence([t[0] for t in batch], padding_value=2)
+    answers = torch.nn.utils.rnn.pad_sequence([t[1] for t in batch], padding_value=2)
+    return questions, answers
 
 if __name__ == '__main__':
     vqa_dataset = VQADataset("../data", "train")
--- unimodal_baseline/language/LSTM.py
+++ unimodal_baseline/language/LSTM.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(0, '../../dataloader')
-from dataset import VQADataset
+from dataset import QADataset, collate_fn_pad
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -39,7 +39,7 @@ class DecoderLSTM(nn.Module):
 
     def forward(self, input, hidden, cell):
         input = input.unsqueeze(0)
-        embs = self.embedding(input)
+        embs = self.word_embeddings(input)
         output, (hidden, cell) = self.lstm(embs, (hidden, cell))
         output = self.linear(output.squeeze(0))
         return output, hidden, cell
@@ -63,40 +63,140 @@ class Seq2Seq(nn.Module):
         outputs = torch.zeros(target_len, batch_size, self.decoder.vocab_size).to(self.device)
         encoder_hidden, encoder_cell = self.encoder(input)
 
-        decoder_input = target[0,:]
+        decoder_input = target[0]
         decoder_hidden = encoder_hidden
         decoder_cell = encoder_cell
 
         loss = 0
         for i in range(1, target_len):
             decoder_output, decoder_hidden, decoder_cell = self.decoder(decoder_input, decoder_hidden, decoder_cell)
-            loss += nn.functional.cross_entropy(decoder_output, target[i])
+            # loss += nn.functional.cross_entropy(decoder_output, target[i])
             outputs[i] = decoder_output
             teacher_forcing = random.random() < teacher_forcing_ratio
             decoder_input = target[i] if teacher_forcing else decoder_output.argmax(dim=1)
             
-        return loss
+        return outputs
+    
+def build_LSTM(encoder_emb_dim, decoder_emb_dim, hidden_dim, vocab_size, n_layers, encoder_dropout, decoder_dropout):
+    enc = EncoderLSTM(encoder_emb_dim, hidden_dim, vocab_size, n_layers, encoder_dropout)
+    dec = DecoderLSTM(decoder_emb_dim, hidden_dim, vocab_size, n_layers, decoder_dropout)
 
-vqa_dataset = VQADataset("../../data", "train")
+    model = Seq2Seq(enc, dec, device).to(device)
 
-VOCAB_SIZE = len(vqa_dataset.vocab)
-ENC_EMB_DIM = 256
-DEC_EMB_DIM = 256
-HID_DIM = 512
-N_LAYERS = 2
-ENC_DROPOUT = 0.5
-DEC_DROPOUT = 0.5
+    def init_weights(m):
+        for _, param in m.named_parameters():
+            if len(param.data.shape)<2:
+                nn.init.uniform_(param.data, -0.08, 0.08)
+            else:
+                nn.init.xavier_uniform_(param.data)
+            
+    model.apply(init_weights)
+    optimizer = optim.Adam(model.parameters())
+    TRG_PAD_IDX = 2
+    criterion = nn.CrossEntropyLoss(ignore_index = TRG_PAD_IDX)
 
-enc = EncoderLSTM(ENC_EMB_DIM, HID_DIM, VOCAB_SIZE, N_LAYERS, ENC_DROPOUT)
-dec = DecoderLSTM(DEC_EMB_DIM, HID_DIM, VOCAB_SIZE, N_LAYERS, DEC_DROPOUT)
+    return model, optimizer, criterion
 
-model = Seq2Seq(enc, dec, device).to(device)
+def print_qa_example(questions, answers, outputs, dataset, batch_idx, len_dataloader, mode):
+    curr_bs = questions.shape[1]
+    num_samples = 10
+    if curr_bs >= num_samples:
+        sample_questions = questions[:, :num_samples].T
+        sample_targets = answers[:, :num_samples].T
+        sample_outputs = outputs[:, :num_samples, :].transpose(0, 1).argmax(dim=-1)
+        print(f'{mode} Batch [{batch_idx + 1}/{len_dataloader}]')
+        for i in range(num_samples):
+            q = dataset.idx_seq_2_sent(sample_questions[i].cpu().numpy())
+            a = dataset.idx_seq_2_sent(sample_outputs[i].cpu().numpy())
+            tg = dataset.idx_seq_2_sent(sample_targets[i].cpu().numpy())
+            print('[Question]: {}\n\t[Answer]: {}\n\t[Target]: {}'.format(q, a, tg))
 
-def init_weights(m):
-    for name, param in m.named_parameters():
-        nn.init.uniform_(param.data, -0.08, 0.08)
-        
-model.apply(init_weights)
-optimizer = optim.Adam(model.parameters())
-TRG_PAD_IDX = TRG.vocab.stoi[TRG.pad_token]
-criterion = nn.CrossEntropyLoss(ignore_index = TRG_PAD_IDX)
+def train(model, dataloader, optimizer, criterion, dataset):
+    total_loss = 0
+    model.train()
+    for batch_idx, (questions, answers) in enumerate(dataloader):
+        # Move data to GPU if available
+        questions = questions.to(device)
+        answers = answers.to(device)
+        # print(questions.T)
+        # print()
+        # print(answers.T)
+        # 1/0
+
+        # Zero the gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(questions, answers)
+
+        # Calculate loss
+        output_dim = outputs.shape[-1]
+        logits = outputs[1:].view(-1, output_dim)
+        labels = answers[1:].view(-1).type(torch.LongTensor).to(device)
+        # print(logits.shape, labels.shape, type(logits), type(labels))
+        loss = criterion(logits, labels)
+
+        # Backward pass
+        loss.backward()
+        optimizer.step()
+
+        # Print loss every 100 batches
+        if (batch_idx + 1) % 100 == 0:
+            print(f'Batch [{batch_idx + 1}/{len(dataloader)}], Loss: {loss.item():.4f}')
+            print_qa_example(questions, answers, outputs, dataset, batch_idx, len(dataloader), 'Train')
+
+        # Accumulate total loss for the epoch
+        total_loss += loss.item()
+    return total_loss / len(dataloader)
+
+
+def eval(model, dataloader, criterion, dataset):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for batch_idx, (questions, answers) in enumerate(dataloader):
+            # Move data to GPU if available
+            questions = questions.to(device)
+            answers = answers.to(device)
+
+            outputs = model(questions, answers)
+
+            # Calculate loss
+            output_dim = outputs.shape[-1]
+            logits = outputs[1:].view(-1, output_dim)
+            labels = answers[1:].view(-1).type(torch.LongTensor).to(device)
+            loss = criterion(logits, labels)
+            total_loss += loss
+
+            if (batch_idx+1) % 60 == 0:
+                print_qa_example(questions, answers, outputs, dataset, batch_idx, len(dataloader), 'Eval')
+    
+    return total_loss / len(dataloader)
+
+
+if __name__ == "__main__":
+
+    qa_dataset_train = QADataset("../../data", "train")
+    qa_dataset_val = QADataset("../../data", "val")
+
+    VOCAB_SIZE = len(qa_dataset_train.vocab)
+    ENC_EMB_DIM = 256
+    DEC_EMB_DIM = 256
+    HID_DIM = 512
+    N_LAYERS = 2
+    ENC_DROPOUT = 0.5
+    DEC_DROPOUT = 0.5
+    BATCH_SIZE = 64
+    NUM_EPOCH = 100
+
+    model, optimizer, criterion = build_LSTM(ENC_EMB_DIM, DEC_EMB_DIM, HID_DIM, VOCAB_SIZE, N_LAYERS, 
+        ENC_DROPOUT, DEC_DROPOUT)
+
+    train_loader = DataLoader(qa_dataset_train, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn_pad)
+    val_loader = DataLoader(qa_dataset_val, batch_size=BATCH_SIZE, shuffle=False, collate_fn=collate_fn_pad)
+
+    for epoch in range(NUM_EPOCH):
+        train_loss = train(model, train_loader, optimizer, criterion, qa_dataset_train)
+        val_loss = eval(model, val_loader, criterion, qa_dataset_val)
+        print('Epoch [{}/{}], Average Training Loss: {:.4f}'.format(epoch + 1, NUM_EPOCH, train_loss))
+        print('Epoch [{}/{}], Average Validation Loss: {:.4f}'.format(epoch + 1, NUM_EPOCH, val_loss))
