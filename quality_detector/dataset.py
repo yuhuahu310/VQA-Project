04004ee04cb7a4df25b5d7f6961a0a4f151d0240
franklinlyj24 <60204460+franklinlyj24@users.noreply.github.com>
2023-04-22 21:07:07 -0400

    Quality detector

--- quality_detector/dataset.py
+++ quality_detector/dataset.py
@@ -1,26 +1,27 @@
 import torch
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
 import os
-import skimage.io as io
-from skimage.transform import resize
 from torchvision import transforms
-import numpy as np
-from vqa import VQA
 from PIL import Image
-from torch.nn import functional as F
 import json
 
 class QDDataset(Dataset):
     def __init__(self, ds_path, phase):
         '''
-        :param ds_path: path to directory that contains quality_detector
+        :param ds_path: path to quality_detector
         :param phase: train, val, or test
         '''
         self.ds_path = ds_path
         self.phase = phase
+        self.preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
         # load dataset
         self.dataset = {}
-        annotation_file = os.path.join(ds_path, 'quality_detector/Annotations', f'{phase}.json')
+        annotation_file = os.path.join(ds_path, 'Annotations', f'{phase}.json')
         if annotation_file != None:
             print('loading dataset into memory...')
             dataset = json.load(open(annotation_file, 'r'))
@@ -37,11 +38,9 @@ class QDDataset(Dataset):
         flaws = []
         for k in sorted(flaws_dict):
             flaws.append(flaws_dict[k])
-        
 
         # Get image and convert to tensor
         img_fpath = os.path.join(self.ds_path, self.phase, image_id)
-        img_arr = io.imread(img_fpath)
-        img_arr = resize(img_arr, (224, 224))
-        img_tensor = torch.tensor(img_arr)
-        img_tensor = torch.permute(img_tensor, (2, 0, 1))
+        input_image = Image.open(img_fpath)
+        input_tensor = self.preprocess(input_image)
+        return input_tensor, torch.Tensor(flaws)
--- quality_detector/model.py
+++ quality_detector/model.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+from dataset import QDDataset
+from torch.utils.data import DataLoader
+import os
 
 class QualityDetector(nn.Module):
     def __init__(self) -> None:
@@ -11,3 +14,98 @@ class QualityDetector(nn.Module):
         x = self.resnext(x)
         x = self.fc(x)
         return x
+
+# Set device to GPU if available, otherwise use CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def train(model, dataloader, optimizer, criterion):
+    total_loss = 0
+    model.train()
+    for batch_idx, (images, targets) in enumerate(dataloader):
+        # Move data to GPU if available
+        images = images.to(device)
+        targets = targets.to(device)
+
+        # Zero the gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        outputs = model(images)
+
+        # Calculate loss
+        loss = criterion(outputs, targets)
+
+        # Backward pass
+        loss.backward()
+        optimizer.step()
+
+        # Print loss every 100 batches
+        if (batch_idx + 1) % 100 == 0:
+            print(f'Batch [{batch_idx + 1}/{len(dataloader)}], Loss: {loss.item():.4f}')
+
+        # Accumulate total loss for the epoch
+        total_loss += loss.item()
+    return total_loss / len(dataloader)
+
+def eval(model, dataloader, criterion):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for batch_idx, (images, targets) in enumerate(dataloader):
+            # Move data to GPU if available
+            images = images.to(device)
+            targets = targets.to(device)
+
+            outputs = model(images)
+
+            # Calculate loss
+            loss = criterion(logits, targets)
+            total_loss += loss.item()
+    return total_loss / len(dataloader)
+
+
+def save_model(model, epoch, save_dir, train_loss, val_loss):
+    path = os.path.join(save_dir, f'QD-epoch_{epoch}-trainloss_{train_loss:.4f}-valloss_{val_loss:.4f}.pth')
+    torch.save(model.state_dict(), path)
+    print(f"Model saved to {path}")
+
+def load_model(model, file_path):
+    model.load_state_dict(torch.load(file_path))
+    print(f"Model loaded from {file_path}")
+
+if __name__ == "__main__":
+    SAVE_DIR = 'ResNext_ckpt'
+    SAVE_FREQ = 5
+    BATCH_SIZE = 64
+    NUM_EPOCH = 50
+    FREEZE_EPOCH = 5
+    dataset_train = QDDataset(".", "train")
+    dataset_val = QDDataset(".", "val")
+    os.makedirs(SAVE_DIR, exist_ok=True)
+
+    model = QualityDetector().to(device)
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.MSELoss()
+    train_loader = DataLoader(dataset_train, batch_size=BATCH_SIZE, shuffle=True)
+    val_loader = DataLoader(dataset_val, batch_size=BATCH_SIZE, shuffle=False)
+
+    TRAIN = True
+    if TRAIN:
+        for epoch in range(NUM_EPOCH):
+            if epoch < FREEZE_EPOCH:
+                model.resnext.requires_grad_(False)
+            else:
+                model.resnext.requires_grad_(True)
+            train_loss = train(model, train_loader, optimizer, criterion, qa_dataset_train)
+            val_loss = eval(model, val_loader, criterion, qa_dataset_val)
+            print('Epoch [{}/{}], Average Training Loss: {:.4f}'.format(epoch + 1, NUM_EPOCH, train_loss))
+            print('Epoch [{}/{}], Average Validation Loss: {:.4f}'.format(epoch + 1, NUM_EPOCH, val_loss))
+            if (epoch + 1) % SAVE_FREQ == 0:
+                save_model(model, epoch, SAVE_DIR, train_loss, val_loss)
+    else:
+        train_loader = DataLoader(dataset_train, batch_size=BATCH_SIZE, shuffle=False)
+        # model = load_model(model, f"{SAVE_DIR}/LSTM-epoch_79-trainloss_1.0620-valloss_5.3343.pth")
+        train_loss = eval(model, train_loader, criterion, dataset_train)
+        val_loss = eval(model, val_loader, criterion, dataset_val)
+        print('Training loss: {:.4f}'.format(train_loss))
+        print('Validation loss: {:.4f}'.format(val_loss))
