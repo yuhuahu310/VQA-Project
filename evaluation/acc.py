7b117a261cada3c8b36139a4f67cf5c840334e4a
franklinlyj24 <60204460+franklinlyj24@users.noreply.github.com>
2023-04-25 02:21:06 -0400

    Unanswerable precision

--- evaluation/acc.py
+++ evaluation/acc.py
@@ -20,6 +20,7 @@ def get_accuracy(path):
     acc_dict = defaultdict(float)
     type_count = defaultdict(int)
     image_ids = set()
+    unans_total = 0
     for i in range(n):
         d = data['data'][i]
         if d['image_id'] in image_ids:
@@ -38,13 +39,18 @@ def get_accuracy(path):
         acc_total += acc
         acc_dict[ans_type] += acc
         type_count[ans_type] += 1
+        if d['predicted_answer'] == 'unanswerable':
+            unans_total += 1
     acc_total /= len(image_ids)
     for ans_type in acc_dict:
         acc_dict[ans_type] /= type_count[ans_type]
+    unans_precision = type_count['unanswerable'] / unans_total if unans_total else float('nan')
     print('Model name: {}\n\tMode: {}\n\tOverall accuracy: {:.4f}'.format(model_name, mode, acc_total))
     print('\tAccuracy by answer type: '+', '.join([k + ': ' + f'{v:.4f}' for k, v in acc_dict.items()]))
+    print(f'\tPrecision of unanswerable: {unans_precision:.4f}')
 
 if __name__ == '__main__':
-    for path in FILENAMES:
+    filenames = ['../gated_clip/gated_clip_full_outputs_{mode}.txt'] # FILENAMES can change to selected files
+    for path in filenames:
         for mode in ['train', 'val']:
             get_accuracy(path.format(mode=mode))
