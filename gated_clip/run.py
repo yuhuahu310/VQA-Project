608f6042066ab67f28824f7716c9c57873bc724c
Kate Hu <katehu310@Kates-MacBook-Pro-2.local>
2023-04-25 01:52:31 -0400

    add answer type prediction in eval

--- gated_clip/run.py
+++ gated_clip/run.py
@@ -12,6 +12,9 @@ import sys
 sys.path.insert(0, '../dataloader')
 from dataset import VQA_mm_Dataset, collate_fn_pad_mm
 
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+
 
 SOS_TOKEN = "<sos>"
 EOS_TOKEN = "<eos>"
@@ -20,10 +23,9 @@ OOV_TOKEN = "<oov>"
 
 
 set_all_seeds(42)
-exp_name = 'case1'
 
-train_dataset = VQA_mm_Dataset("../../../../net/projects/ranalab/kh310/vqa", "train", subset=True)
-qa_dataset_val = VQA_mm_Dataset("../../../../net/projects/ranalab/kh310/vqa", "val", subset=True)
+train_dataset = VQA_mm_Dataset("../../../../net/projects/ranalab/kh310/vqa", "train")
+qa_dataset_val = VQA_mm_Dataset("../../../../net/projects/ranalab/kh310/vqa", "val")
 
 VOCAB_SIZE = len(train_dataset.vocab)
 ENC_EMB_DIM = 1000
@@ -59,50 +61,57 @@ trainer = Trainer(transformer, train_dataloader, val_dataloader,
           device = device
         )
 
-# trainer.train()
+trainer.train()
 
-# torch.save(transformer.state_dict(), "/home/yizhi/VQA-Project/gated_clip/model.pth")
-
-transformer.load_state_dict(torch.load("/home/yizhi/VQA-Project/gated_clip/model.pth"))
+# # torch.save(transformer.state_dict(), "/home/yizhi/VQA-Project/gated_clip/model.pth")
+#
+exp_name = 'gated_clip_1'
+transformer.load_state_dict(torch.load(f"./model_{exp_name}.pth"))
 transformer.eval()
-
-
-
-# Plot the training losses.
-# plt.plot(trainer.loss_history)
-# plt.xlabel('Iteration')
-# plt.ylabel('Loss')
-# os.makedirs('plots', exist_ok=True)
-# plt.title('Training loss history')
-# plt.savefig('plots/' + exp_name + '_loss_out.png')
-
-# plt.close()
-# plt.plot(trainer.val_loss_history)
-# plt.xlabel('Iteration')
-# plt.ylabel('Loss')
-# os.makedirs('plots', exist_ok=True)
-# plt.title('Validaion loss history')
-# plt.savefig('plots/' + exp_name + '_val_loss_out.png')
-
-
-# vis_imgs('train')
-# vis_imgs('val')
-
-
+#
+#
+#
+# # Plot the training losses.
+# # plt.plot(trainer.loss_history)
+# # plt.xlabel('Iteration')
+# # plt.ylabel('Loss')
+# # os.makedirs('plots', exist_ok=True)
+# # plt.title('Training loss history')
+# # plt.savefig('plots/' + exp_name + '_loss_out.png')
+#
+# # plt.close()
+# # plt.plot(trainer.val_loss_history)
+# # plt.xlabel('Iteration')
+# # plt.ylabel('Loss')
+# # os.makedirs('plots', exist_ok=True)
+# # plt.title('Validaion loss history')
+# # plt.savefig('plots/' + exp_name + '_val_loss_out.png')
+#
+#
+# # vis_imgs('train')
+# # vis_imgs('val')
+#
+#
 def eval(model, model_name, loader, criterion, split='val', dump=''):
+
+    type2text = ['unanswerable', 'yes/no', 'number', 'other']
+
     all_image_ids = []
     all_questions = []
     all_targets = []
     all_predictions = []
+    all_ans_types = []
+    all_ans_types_gt = []
     total_loss = 0
     for batch in loader:
         features, questions, answers, anstypes, image_ids, questions_org = batch
         gt_answers = decode_captions(answers.numpy(), train_dataset.reverse_vocab)
         logits = model(features.float().cuda(), questions.cuda(), answers[:,:-1].cuda())
+        scores, answer_type = logits
+        ans_type_pred = torch.argmax(F.softmax(answer_type, dim=1), 1) # (B,)
         final_captions, final_logits, predicted_answer_type_logits = model.sample(features.float().cuda(), questions.cuda(), max_length=answers.shape[1]-1)
         sample_answers = decode_captions(final_captions, train_dataset.reverse_vocab)
         gt_questions = decode_captions(questions_org.numpy(), train_dataset.reverse_vocab)
-        import pdb; pdb.set_trace()
         loss, language_loss, answer_type_loss = criterion((final_logits, predicted_answer_type_logits), answers[:, 1:].long().to("cuda"), anstypes.to("cuda"))
         total_loss += loss
 
@@ -110,7 +119,9 @@ def eval(model, model_name, loader, criterion, split='val', dump=''):
         all_questions.extend(gt_questions)
         all_targets.extend(gt_answers)
         all_predictions.extend(sample_answers)
-      
+        all_ans_types.extend(ans_type_pred)
+        all_ans_types_gt.extend(anstypes)
+
     if dump:
         out = {
             "model_name": model_name,
@@ -120,34 +131,36 @@ def eval(model, model_name, loader, criterion, split='val', dump=''):
             },
             "data": []
         }
-        assert len(all_image_ids) == len(all_questions) == len(all_targets) == len(all_predictions)
-        for img, q, tg, pred in zip(all_image_ids, all_questions, all_targets, all_predictions):
+        assert len(all_image_ids) == len(all_questions) == len(all_targets) == len(all_predictions) == len(all_ans_types) == len(all_ans_types_gt)
+        for img, q, tg, pred, anstype_pred, anstype in zip(all_image_ids, all_questions, all_targets, all_predictions, all_ans_types, all_ans_types_gt):
             out["data"].append({
                 "image_id": img.replace(".jpg", ""),
                 "question": q,
                 "predicted_answer": pred,
-                "target_answer": tg
+                "target_answer": tg,
+                "predicted_answer_type": type2text[anstype_pred],
+                "target_answer_type": type2text[anstype]
             })
 
         with open(dump, 'w') as f:
             json.dump(out, f, indent=4)
+        f.close()
 
     return total_loss / len(loader)
 
 
 import torch.nn.functional as F
 def criterion(predictions, answers, answer_type):
-    
+
       predicted_answers, predicted_answer_type = predictions
       mask = answer_type != 0
       answers = answers.long()
       masked_logit = predicted_answers * mask.unsqueeze(1).broadcast_to(predicted_answers.shape[:2]).reshape(list(predicted_answers.shape[:2]) + [1])
-      language_loss = F.cross_entropy(masked_logit.reshape(-1, masked_logit.size(-1)), 
+      language_loss = F.cross_entropy(masked_logit.reshape(-1, masked_logit.size(-1)),
                                       answers.reshape(-1), ignore_index=2)
       answer_type_loss = F.cross_entropy(predicted_answer_type, answer_type)
       total_loss = language_loss + answer_type_loss
       return total_loss, language_loss, answer_type_loss
-exp_name = "gated_clip"
 
 loss = eval(transformer, exp_name, val_dataloader, criterion, split='val', dump='%s_outputs_val.txt' % exp_name)
 print("%s loss:" % exp_name, loss)
